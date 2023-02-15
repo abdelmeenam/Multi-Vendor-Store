381514ab548c7babf58ce98ed0203f476f00e19c
abdelmeenam <abdelmeenam@gmail.com>
2023-02-15 18:21:46 +0200

    Auth

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
-    public function index()
+    public function index(Request $request  )
     {
+
+
             $user = 'Abdelmonem Mohmed';
             return view('Admin.index' , compact(['user']));
     }
