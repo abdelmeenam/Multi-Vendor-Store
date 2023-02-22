15c4b82de8fe4802b0b1612fa18806453368d036
abdelmeenam <abdelmeenam@gmail.com>
2023-02-22 20:27:12 +0200

    Validations

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -26,6 +26,7 @@ class ProfileController extends Controller
      */
     public function update(ProfileUpdateRequest $request): RedirectResponse
     {
+        //check
         $request->user()->fill($request->validated());
 
         if ($request->user()->isDirty('email')) {
