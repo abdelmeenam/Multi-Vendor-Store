7c8c39a0fdd3ad06316df2a382be11930ec5a9f4
abdelmeenam <abdelmeenam@gmail.com>
2023-04-04 22:34:39 +0200

    Real time Notifications

--- resources/views/Admin/Layouts/dashboard.blade.php
+++ resources/views/Admin/Layouts/dashboard.blade.php
@@ -241,7 +241,7 @@
                 Anything you want
             </div>
             <!-- Default to the left -->
-            <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights
+            <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All Rights
             reserved.
         </footer>
 
