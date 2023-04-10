f58eb5c31a30d60e54e83958c65f0482122cae4d
abdelmeenam <abdelmeenam@gmail.com>
2023-04-10 19:02:20 +0200

    cart update

--- app/Observers/CartObserver.php
+++ app/Observers/CartObserver.php
@@ -19,47 +19,4 @@ class CartObserver
         $cart->cookie_id = cart::getCookieId();
     }
 
-    /**
-     * Handle the cart "updated" event.
-     *
-     * @param  \App\Models\cart  $cart
-     * @return void
-     */
-    public function updated(cart $cart)
-    {
-        //
-    }
-
-    /**
-     * Handle the cart "deleted" event.
-     *
-     * @param  \App\Models\cart  $cart
-     * @return void
-     */
-    public function deleted(cart $cart)
-    {
-        //
-    }
-
-    /**
-     * Handle the cart "restored" event.
-     *
-     * @param  \App\Models\cart  $cart
-     * @return void
-     */
-    public function restored(cart $cart)
-    {
-        //
-    }
-
-    /**
-     * Handle the cart "force deleted" event.
-     *
-     * @param  \App\Models\cart  $cart
-     * @return void
-     */
-    public function forceDeleted(cart $cart)
-    {
-        //
-    }
 }
